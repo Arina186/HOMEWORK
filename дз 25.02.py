b28f14fd1b4668d8b99e546b09670d89f3f7c02f
Arina186 <ariadna.sem@mail.ru>
2026-02-26 20:13:53 +0300

    Task 2 min и max в матрице25.02

--- дз 25.02.py
+++ дз 25.02.py
@@ -46,4 +46,61 @@ while is_continue:
         print(f" \nВаша матрица:")
         show_matrix(result)
 
+    user_choice = input("Enter action number: ")
+    if user_choice == '2':
+        from random import randint
 
+
+        def generate_random_matrix(rows_count, columns_count):
+            matrix = []
+            for row in range(0, rows_count):
+                matrix.append([])
+                for col in range(0, columns_count):
+                    matrix[row].append(randint(0, 50))
+
+            return matrix
+
+
+        def show_matrix(matrix):
+            for row in matrix:
+                print(row)
+
+            return matrix
+
+
+        def min_value_matrix(matrix):
+            # берем первый элемент 1 строки матрицы как начальный минимум
+            minimum = matrix[0][0]
+            min_row_index, min_col_index = 0, 0  #для отображения индекса найденного минимума
+            for r_ind, row in enumerate(matrix):  #enumerate возвращает индекс и само значение
+                for c_ind, item in enumerate(row):
+                    if item<minimum:
+                        minimum = item
+                        min_row_index = r_ind
+                        min_col_index = c_ind
+
+            return minimum, (min_row_index, min_col_index)     # взяв в скобки мы сгруппировали неск значений в кортеж
+# если не возьмем, то придется ниже брать 3 переменные (min_value, r_index, c_index)вместо 2-х (min_value, min_index)
+
+        def max_value_matrix(matrix):
+            maximum = matrix[0][0]
+            max_row_index, max_col_index = 0, 0
+            for r_ind, row in enumerate(matrix):
+                for c_ind, item in enumerate(row):
+                    if item>maximum:
+                        maximum = item
+                        max_row_index = r_ind
+                        max_col_index = c_ind
+
+            return maximum, (max_row_index, max_col_index)
+
+
+        rows_number = get_int_input("Enter rows number: ")
+        columns_number = get_int_input("Enter columns number: ")
+        result = generate_random_matrix(rows_number, columns_number)
+        print(f" \nВаша матрица:")
+        show_matrix(result)
+        min_value, min_index = min_value_matrix(result)
+        max_value, max_index = max_value_matrix(result)
+        print(f"Минимальное значение матрицы: {min_value}, индекс: {min_index}")
+        print(f"Максимальное значение матрицы: {max_value}, индекс: {max_index}")
