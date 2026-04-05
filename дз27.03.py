607ba6a7713c28864cf252c208bdc992c6a50a07
Arina186 <ariadna.sem@mail.ru>
2026-04-05 18:12:20 +0300

    переделанное error и числа

--- дз27.03.py
+++ дз27.03.py
@@ -7,8 +7,10 @@ def get_int_input(prompt):
             print("Введите целое число!")
 
 
-def filter_text(file_path, level):
-    target_lev = f"[{level}]"
+def filter_text(file_path, levels):
+    if isinstance(levels, str):
+        levels = [levels]
+    target_prefixes = [f"[{lvl.strip().upper()}]" for lvl in levels]
     current_block = []
     is_target_level = False
     with open(file_path, "r") as file:
@@ -20,7 +22,11 @@ def filter_text(file_path, level):
                     yield current_block
 
                 current_block = []
-                is_target_level = stripped_line.startswith(target_lev)
+                is_target_level = False
+                for p in target_prefixes:
+                    if stripped_line.startswith(p):
+                        is_target_level = True
+                        break
 
             if is_target_level and stripped_line:
                 current_block.append(stripped_line)
@@ -49,14 +55,25 @@ while is_continue:
 
     user_choice = get_int_input("Enter action number: ")
     if user_choice == 1:
-        filter_generator = filter_text("inform27.03.txt", "INFO")
+        level_input = input("Введите уровень или уровни через пробел (например: INFO ERROR): ")
+        selected_levels = level_input.replace(",", " ").split()
+
+        if not selected_levels:
+            print("Вы ничего не ввели!")
+            continue
+        filter_generator = filter_text("inform27.03.txt", selected_levels)
+        found_anything = False
         for item in filter_generator:
+            found_anything = True
             print("\n".join(item))
+
+        if not found_anything:
+            print(f"Записей с уровнями {selected_levels} не обнаружено.")
 
     elif user_choice == 2:
         numbers = [1, 1, 1, 1, 2, 2, 3, 3, 3, 4, 5, 5, 5, 5, 6, 7, 7]
         for basket in group_repeating(numbers):
-            print(basket)
+            print(f"\n Группировка списка {basket}")
 
     elif user_choice == 3:
         print("Выход из программы...")
